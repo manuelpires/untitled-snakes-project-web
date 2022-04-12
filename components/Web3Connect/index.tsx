bd358552e823db9d8421ef16d05adf20e5fde8d0
manuelpires <manuelpiresok@gmail.com>
2022-04-12 18:58:34 +0200

    Update Web3Connect component with children props

--- components/Web3Connect/index.tsx
+++ components/Web3Connect/index.tsx
@@ -1,17 +1,20 @@
-import { useState, useEffect } from "react";
+import { ReactNode, useState, useEffect } from "react";
 import { useWeb3React } from "@web3-react/core";
 import { UserRejectedRequestError } from "@web3-react/injected-connector";
 import { toast } from "react-toastify";
-import Button from "../Button";
-import useMetaMaskOnboarding from "../../hooks/useMetaMaskOnboarding";
 import { injected } from "../../connectors";
-import styles from "./Web3Connect.module.css";
+import useEagerConnect from "../../hooks/useEagerConnect";
+import useMetaMaskOnboarding from "../../hooks/useMetaMaskOnboarding";
+import Button from "../Button";
 
 type Props = {
-  triedToEagerConnect: boolean;
+  children?: ReactNode;
+  isSmall?: boolean;
 };
 
-const Web3Connect = ({ triedToEagerConnect }: Props) => {
+const Web3Connect = ({ children, isSmall = false }: Props) => {
+  const triedToEagerConnect = useEagerConnect();
+
   const { account, activate, active, error, setError } = useWeb3React();
 
   const {
@@ -39,35 +42,45 @@ const Web3Connect = ({ triedToEagerConnect }: Props) => {
     }
   }, [active, error, stopOnboarding]);
 
-  if (!triedToEagerConnect || typeof account === "string") {
+  // Case 1: Didn't try to connect yet (loading)
+  if (!triedToEagerConnect) {
     return null;
   }
 
-  return (
-    <section className={styles.section}>
-      {isWeb3Available ? (
-        <Button
-          disabled={connecting}
-          onClick={() => {
-            setConnecting(true);
+  // Case 2: Tried to connect but still not connected
+  if (typeof account !== "string") {
+    return (
+      <>
+        {isWeb3Available ? (
+          <Button
+            isSmall={isSmall}
+            disabled={connecting}
+            onClick={() => {
+              setConnecting(true);
 
-            activate(injected, undefined, true).catch((error) => {
-              // ignore the error if it's a user rejected request
-              if (error instanceof UserRejectedRequestError) {
-                setConnecting(false);
-              } else {
-                setError(error);
-              }
-            });
-          }}
-        >
-          {isMetaMaskInstalled ? "Connect to MetaMask" : "Connect to Wallet"}
-        </Button>
-      ) : (
-        <Button onClick={startOnboarding}>Install MetaMask</Button>
-      )}
-    </section>
-  );
+              activate(injected, undefined, true).catch((error) => {
+                // ignore the error if it's a user rejected request
+                if (error instanceof UserRejectedRequestError) {
+                  setConnecting(false);
+                } else {
+                  setError(error);
+                }
+              });
+            }}
+          >
+            {isMetaMaskInstalled ? "Connect to MetaMask" : "Connect to Wallet"}
+          </Button>
+        ) : (
+          <Button isSmall={isSmall} onClick={startOnboarding}>
+            Install MetaMask
+          </Button>
+        )}
+      </>
+    );
+  }
+
+  // Case 3: Connected successfully: render children
+  return <>{children}</>;
 };
 
 export default Web3Connect;

71be65e83f8ab122ede0d7244aa2d179b97871ee
manuelpires <manuelpiresok@gmail.com>
2022-04-12 18:53:32 +0200

    Create AccountLink component to display user address

---
+++ components/AccountLink/index.tsx
@@ -0,0 +1,37 @@
+import { useMemo } from "react";
+import { useWeb3React } from "@web3-react/core";
+import {
+  shortenHex,
+  formatEtherscanLink,
+  EtherscanLinkType,
+} from "../../utils";
+import useENSName from "../../hooks/useENSName";
+import ExternalLink from "../ExternalLink";
+import Button from "../Button";
+
+const AccountLink = () => {
+  const { account, chainId } = useWeb3React();
+
+  const ENSName = useENSName(account);
+
+  const accountLink = useMemo(
+    () =>
+      account && chainId
+        ? formatEtherscanLink(EtherscanLinkType.Account, [chainId, account])
+        : undefined,
+    [account, chainId]
+  );
+
+  const accountShort = useMemo(
+    () => (account ? shortenHex(account) : undefined),
+    [account]
+  );
+
+  return (
+    <ExternalLink url={accountLink}>
+      <Button isSmall>{ENSName || accountShort}</Button>
+    </ExternalLink>
+  );
+};
+
+export default AccountLink;

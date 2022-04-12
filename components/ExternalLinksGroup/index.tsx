b1e4f41fd980e98c21678c02cf70f91f584f2eea
manuelpires <manuelpiresok@gmail.com>
2022-04-12 18:56:00 +0200

    Create ExternalLinksGroup component and use it on Footer

---
+++ components/ExternalLinksGroup/index.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import { externalLinks } from "../../config";
+import ExternalLink from "../ExternalLink";
+import styles from "./ExternalLinksGroup.module.css";
+
+const ExternalLinksGroup = () => (
+  <div className={styles.container}>
+    {externalLinks.map(({ url, image, alt }) => (
+      <ExternalLink key={alt} url={url}>
+        <Image src={image} alt={alt} layout="fixed" />
+      </ExternalLink>
+    ))}
+  </div>
+);
+
+export default ExternalLinksGroup;
--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,35 +1,10 @@
-import { useMemo } from "react";
-import Image from "next/image";
-import Link from "../Link";
-import { formatEtherscanLink, EtherscanLinkType } from "../../utils";
-import OpenSeaLogo from "../../public/icons/opensea-logo.svg";
-import EtherscanLogo from "../../public/icons/etherscan-logo.svg";
-import TwitterLogo from "../../public/icons/twitter-logo.svg";
 import styles from "./Footer.module.css";
+import ExternalLinksGroup from "../ExternalLinksGroup";
 
-const Footer = () => {
-  const etherscanLink = useMemo(
-    () =>
-      formatEtherscanLink(EtherscanLinkType.Contract, [
-        Number(process.env.NEXT_PUBLIC_CHAIN_ID),
-        process.env.NEXT_PUBLIC_CONTRACT_ADDRESS as string,
-      ]),
-    []
-  );
-
-  return (
-    <footer className={styles.footer}>
-      <Link url="https://twitter.com/snakesproject">
-        <Image src={TwitterLogo} alt="Twitter" width={32} height={32} />
-      </Link>
-      <Link url="https://opensea.io/collection/snakesproject">
-        <Image src={OpenSeaLogo} alt="OpenSea" width={32} height={32} />
-      </Link>
-      <Link url={etherscanLink}>
-        <Image src={EtherscanLogo} alt="Etherscan" width={32} height={32} />
-      </Link>
-    </footer>
-  );
-};
+const Footer = () => (
+  <footer className={styles.footer}>
+    <ExternalLinksGroup />
+  </footer>
+);
 
 export default Footer;

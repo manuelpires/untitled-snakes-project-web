1e73e5f772c1622c79ceb8c6b71bc5965ea3fa29
manuelpires <manuelpiresok@gmail.com>
2022-04-12 18:50:36 +0200

    Add navbar and external links config

---
+++ config.ts
@@ -0,0 +1,44 @@
+import OpenSeaLogo from "./public/icons/opensea-logo.svg";
+import EtherscanLogo from "./public/icons/etherscan-logo.svg";
+import TwitterLogo from "./public/icons/twitter-logo.svg";
+import { formatEtherscanLink, EtherscanLinkType } from "./utils";
+
+export const navBarLinks = [
+  {
+    href: "/",
+    label: "Home",
+  },
+  {
+    href: "#mint",
+    label: "Mint",
+  },
+  {
+    href: "#about",
+    label: "About",
+  },
+  {
+    href: "#team",
+    label: "Team",
+  },
+];
+
+export const externalLinks = [
+  {
+    url: "https://twitter.com/snakesproject",
+    image: TwitterLogo,
+    alt: "Twitter",
+  },
+  {
+    url: "https://opensea.io/collection/snakesproject",
+    image: OpenSeaLogo,
+    alt: "OpenSea",
+  },
+  {
+    url: formatEtherscanLink(EtherscanLinkType.Contract, [
+      Number(process.env.NEXT_PUBLIC_CHAIN_ID),
+      process.env.NEXT_PUBLIC_CONTRACT_ADDRESS as string,
+    ]),
+    image: EtherscanLogo,
+    alt: "Etherscan",
+  },
+];

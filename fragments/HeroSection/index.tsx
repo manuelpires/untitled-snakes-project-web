b617206af7a90da326cd3e3e2c7e7d03133cf4cc
manuelpires <manuelpiresok@gmail.com>
2022-04-13 15:55:11 +0200

    Add Title component

--- fragments/HeroSection/index.tsx
+++ fragments/HeroSection/index.tsx
@@ -1,10 +1,11 @@
+import Title from "../../components/Title";
 import HeroImages from "../../components/HeroImages";
 import styles from "./HeroSection.module.css";
 
 const HeroSection = () => (
   <section className={styles.container}>
     <header className={styles.header}>
-      <h1 className={styles.title}>Untitled Snakes Project</h1>
+      <Title isMainTitle text="Untitled Snakes Project" />
     </header>
     <HeroImages />
   </section>
--- fragments/MintSection/index.tsx
+++ fragments/MintSection/index.tsx
@@ -1,9 +1,13 @@
+import Title from "../../components/Title";
 import Web3Connect from "../../components/Web3Connect";
 import MintPanel from "../../components/MintPanel";
 import styles from "./MintSection.module.css";
 
 const MintSection = () => (
   <section id="mint" className={styles.container}>
+    <header>
+      <Title text="Mint" />
+    </header>
     <Web3Connect>
       <MintPanel />
     </Web3Connect>

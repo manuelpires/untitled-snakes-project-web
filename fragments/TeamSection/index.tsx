bf2b372b4a99a0abdbc05729b5d7c0f8fae2280a
manuelpires <manuelpiresok@gmail.com>
2022-04-14 15:42:52 +0200

    Update TeamSection with team member card

--- fragments/TeamSection/index.tsx
+++ fragments/TeamSection/index.tsx
@@ -1,8 +1,18 @@
+import shakaImage from "../../public/images/snake-4.png";
 import Section from "../Section";
+import MemberCard from "../../components/MemberCard";
+import styles from "./TeamSection.module.css";
 
 const TeamSection = () => (
   <Section id="team" title="Team">
-    
+    <div className={styles.container}>
+      <MemberCard
+        name="Shaka"
+        bio={"Code + Pixels.\nCreator of Snakes.\nNot good at naming projects."}
+        twitterLink="https://twitter.com/shakazaitsev"
+        image={shakaImage}
+      />
+    </div>
   </Section>
 );
 

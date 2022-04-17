0d8900ae9bacab8a27a92119126a40141e399025
manuelpires <manuelpiresok@gmail.com>
2022-04-17 02:06:40 +0200

    Update API Route

--- pages/api/snake/[tokenId].ts
+++ pages/api/snake/[tokenId].ts
@@ -1,21 +1,21 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import middleware from "../../../../middleware";
+import middleware from "../../../middleware";
 
-// GET /api/snakes/image/:tokenId
+// GET /api/snake/:tokenId
 const handler = async (
   { query: { tokenId } }: NextApiRequest,
   res: NextApiResponse
 ) => {
   try {
     const response = await fetch(
-      `${process.env.IPFS_IMAGES_BASE_URI}${tokenId}.png`
+      `${process.env.IPFS_METADATA_BASE_URI}${tokenId}`
     );
-    const arrayBuffer = await response.arrayBuffer();
+    const json = await response.json();
+    json.image = `${process.env.NEXT_PUBLIC_IMAGES_BASE_URI}${tokenId}`;
     res
-      .setHeader("Content-Type", "image/png")
       .setHeader("Cache-Control", "max-age=0, s-maxage=31536000")
       .status(200)
-      .end(Buffer.from(arrayBuffer));
+      .json(json);
   } catch (err) {
     res.status(500).end();
   }
--- pages/api/snake/image/[tokenId].ts
+++ pages/api/snake/image/[tokenId].ts
@@ -1,21 +1,21 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import middleware from "../../../middleware";
+import middleware from "../../../../middleware";
 
-// GET /api/snakes/:tokenId
+// GET /api/snake/image/:tokenId
 const handler = async (
   { query: { tokenId } }: NextApiRequest,
   res: NextApiResponse
 ) => {
   try {
     const response = await fetch(
-      `${process.env.IPFS_METADATA_BASE_URI}${tokenId}`
+      `${process.env.IPFS_IMAGES_BASE_URI}${tokenId}.png`
     );
-    const json = await response.json();
-    json.image = `${process.env.NEXT_PUBLIC_IMAGES_BASE_URI}${tokenId}`;
+    const arrayBuffer = await response.arrayBuffer();
     res
+      .setHeader("Content-Type", "image/png")
       .setHeader("Cache-Control", "max-age=0, s-maxage=31536000")
       .status(200)
-      .json(json);
+      .end(Buffer.from(arrayBuffer));
   } catch (err) {
     res.status(500).end();
   }
